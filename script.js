356c048e7e4ec730e98d1495b76b3ff436324d27
Emuolanari <emuolanari@gmail.com>
2021-03-05 12:21:47 +0000

    correct number of males and females

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ window.addEventListener('load',function(){
             for (const key in studentDetails) {
                 //console.log(`${key}: ${studentDetails[key]}`);
                 //const propertyId = Object.keys(studentDetails);
-                let [id,gender] = [studentDetails.ID,studentDetails.Gender];
-                genderArray.push({id,gender});
+                let [studentId,gender] = [studentDetails.StudentID,studentDetails.Gender];
+                genderArray.push({studentId,gender});
             }  
         }
         const uniqueGenderSet = new Set(genderArray.map(e => JSON.stringify(e)));
@@ -45,7 +45,7 @@ window.addEventListener('load',function(){
         //console.log(maxY);
         const xScale = d3.scaleBand().domain(numberBasedOnGender.map((d)=>d.key))
                 .range([100,400]).padding(0.1);
-        const yScale = d3.scaleLinear().domain([0,maxY+2000]).range([500,50]);
+        const yScale = d3.scaleLinear().domain([0,maxY]).range([500,50]);
 
         const xAxsi = d3.axisBottom().scale(xScale);
         const yAxsi = d3.axisLeft().scale(yScale);
